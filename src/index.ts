6bc0f38ffe7d7b21e1b71e28910f4f88b59a2517
Alistair Scott <ali.scott@gmail.com>
2021-11-25 10:01:16 +0000

    fix: exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-export { autodetect } from './platforms/autodetect';
-export { AzureDevOps } from './platforms/azureDevOps';
-export { GitHub } from './platforms/github';
-export { GitLab } from './platforms/gitlab';
+import { autodetect } from './platforms/autodetect';
+import { AzureDevOps } from './platforms/azureDevOps';
+import { GitHub } from './platforms/github';
+import { GitLab } from './platforms/gitlab';
+
+export default {
+  autodetect,
+  AzureDevOps,
+  GitHub,
+  GitLab,
+};
+
 export * from './types';
