f56f3a419b53791792b31b456745047919681b84
Alistair Scott <ali.scott@gmail.com>
2021-11-25 09:14:00 +0000

    refactor: export types at top-level

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-import { autodetect } from './platforms/autodetect';
-import { AzureDevOps } from './platforms/azureDevOps';
-import { GitHub } from './platforms/github';
-import { GitLab } from './platforms/gitlab';
-
-export default {
-  autodetect,
-  GitHub,
-  GitLab,
-  AzureDevOps,
-};
+export { autodetect } from './platforms/autodetect';
+export { AzureDevOps } from './platforms/azureDevOps';
+export { GitHub } from './platforms/github';
+export { GitLab } from './platforms/gitlab';
+export * from './types';
